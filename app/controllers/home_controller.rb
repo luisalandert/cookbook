b4e05e8ec7ba66fe5b9b235af5870c6083a2a52c
luisalandert <luludila@gmail.com>
2020-08-04 19:06:02 -0300

    index view created

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+    def index
+        # render plain: 'TREINADEV'
+        # render :index #usar se essa action for rodar outra view que nao for index
+    end
+end
