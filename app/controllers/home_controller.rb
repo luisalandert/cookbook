67cb99afe43043a32ba517ff150993beb51e431a
luisalandert <luludila@gmail.com>
2020-08-04 22:11:33 -0300

    database created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
     def index
         # render plain: 'TREINADEV'
         # render :index #usar se essa action for rodar outra view que nao for index
+        @recipes = Recipe.all
     end
 end
